76cc8f9aa8b00915f3099dc0b2a27175bf603a61
lister284 <johnlister284@gmail.com>
2016-01-30 12:53:30 -0500

    options.js

--- lambda/src/options.example.js
+++ lambda/src/options.example.js
@@ -1,6 +1,6 @@
 var options = {
-  appid: "amzn1.echo-sdk-ams.app.ENTER_YOUR_APP_ID_FOR_ECHO_HERE",
-  host: "host_for_sonos_api",
+  appid: "amzn1.echo-sdk-ams.app.90e763c6-067c-4164-9f6f-513013454960",
+  host: "listernet.com",
   port: "5005"
 };
 
